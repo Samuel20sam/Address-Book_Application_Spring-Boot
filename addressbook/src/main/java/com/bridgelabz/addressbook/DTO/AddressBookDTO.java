87d32a970b8d17e8c6f05b51c1f2cecc97ec788a
Samuel_Elijah <samuel20sam@gmail.com>
2022-08-29 11:57:56 +0530

    Adding: Introducing DTO and Model to Address Book Application

---
+++ addressbook/src/main/java/com/bridgelabz/addressbook/DTO/AddressBookDTO.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.addressbook.DTO;
+
+public class AddressBookDTO {
+    public String firstName;
+    public String lastName;
+    public String gender;
+//    @JsonFormat(pattern = "dd-MM-yyyy")
+//    public LocalDate dOB;
+    public String emailID;
+    public long phoneNumber;
+    public long alternatePhoneNumber;
+    public String address;
+    public String city;
+    public String state;
+    public int pincode;
+
+    public AddressBookDTO(String firstName, String lastName, String gender, //LocalDate dOD,
+                          String emailID, long phoneNumber, long alternatePhoneNumber, String address, String city,
+                          String state, int pincode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+//        this.dOB = dOD;
+        this.emailID = emailID;
+        this.phoneNumber = phoneNumber;
+        this.alternatePhoneNumber = alternatePhoneNumber;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.pincode = pincode;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBookDTO{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+//                ", dOB=" + dOB +
+                ", emailID='" + emailID + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", alternatePhoneNumber=" + alternatePhoneNumber +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", pincode=" + pincode +
+                '}';
+    }
+}
---
+++ addressbook/src/main/java/com/bridgelabz/addressbook/model/AddressBookData.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.addressbook.model;
+
+import com.bridgelabz.addressbook.DTO.AddressBookDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddressBookData {
+    private int addressBookID;
+    private String firstName;
+    private String lastName;
+    private String gender;
+//    @JsonFormat(pattern = "dd-MM-yyyy")
+//    private LocalDate dOB;
+    private String emailID;
+    private long phoneNumber;
+    private long alternatePhoneNumber;
+    private String address;
+    private String city;
+    private String state;
+    private long pincode;
+
+    public AddressBookData(int addressBookID, AddressBookDTO address_book_dto) {
+        this.addressBookID = addressBookID;
+        this.firstName = address_book_dto.firstName;
+        this.lastName = address_book_dto.lastName;
+        this.gender = address_book_dto.gender;
+//        this.dOB = address_book_dto.dOB;
+        this.emailID = address_book_dto.emailID;
+        this.phoneNumber = address_book_dto.phoneNumber;
+        this.alternatePhoneNumber = address_book_dto.alternatePhoneNumber;
+        this.address = address_book_dto.address;
+        this.city = address_book_dto.city;
+        this.state = address_book_dto.state;
+        this.pincode = address_book_dto.pincode;
+    }
+}
