a1f7dc79c5e92478097bf59ee554a3dc420c0584
Samuel_Elijah <samuel20sam@gmail.com>
2022-08-29 13:30:29 +0530

    feat: using lambok '@Slf4j' for Logging Adress Book Application

--- addressbook/src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
+++ addressbook/src/main/java/com/bridgelabz/addressbook/AddressBookApplication.java
@@ -1,13 +1,17 @@
 package com.bridgelabz.addressbook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(AddressBookApplication.class, args);
+        log.info("AddressBook App Started in {} Environment", applicationContext.getEnvironment().getProperty("environment"));
+        log.info("AddressBook App started");
 		System.out.println("\n\n ------ Welcome to Address Book Application ------");
-	}
+    }
 }
