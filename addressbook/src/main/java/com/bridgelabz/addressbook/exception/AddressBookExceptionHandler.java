77077e44cf7ce448bcdff276d12b78f5d7049095
Samuel_Elijah <samuel20sam@gmail.com>
2022-08-30 12:17:58 +0530

    mod: adding otherional variables and validation in model and dto layer

--- addressbook/src/main/java/com/bridgelabz/addressbook/exception/AddressBookExceptionHandler.java
+++ addressbook/src/main/java/com/bridgelabz/addressbook/exception/AddressBookExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.addressbook.exception;
 
 import com.bridgelabz.addressbook.DTO.ResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,19 +15,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@Slf4j
 public class AddressBookExceptionHandler {
+    private static final String message = "Exception While processing REST Request";
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
         List<String> errorMessage = errorList.stream().map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
-        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errorMessage);
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+        ResponseDTO responseDTO = new ResponseDTO(message, errorMessage);
+        return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(AddressBookException.class)
-    public ResponseEntity<ResponseDTO> handleAddressBookException(AddressBookException exception){
-        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",exception.getMessage());
+    public ResponseEntity<ResponseDTO> handleAddressBookException(AddressBookException exception) {
+        ResponseDTO responseDTO = new ResponseDTO(message, exception.getMessage());
         return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        log.error("Invalid Date format", exception);
+        ResponseDTO responseDTO = new ResponseDTO(message, "Date should be in the format of dd-MM-yyyy");
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 }
